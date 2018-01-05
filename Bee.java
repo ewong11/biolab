ca43da8c703e52d579f69c7ff86de152c2fea9e0
KaitlinWan <kwan1@stuy.edu>
2018-01-05 09:24:01 -0500

    added bee

---
+++ Bee.java
@@ -0,0 +1,9 @@
+/*********************************
+class Bee extends Student
+**********************************/
+public class Bee extends Student
+{
+    public Bee() {
+	intel = 40;
+	friendCount = 7;
+	
