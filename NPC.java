ec5e511065eb7832706f4a5a3021c171ac8e5d97
Kaitlin <kwan1@stuy.edu>
2018-01-05 09:15:53 -0500

    added NPC

---
+++ NPC.java
@@ -0,0 +1,15 @@
+public class NPC extends Character{
+    protected static int friendship;
+
+    public int getFriendship(){
+	return friendship;
+    }
+
+
+    public void setFriendship(int n){
+	friendship = n;
+    }
+
+
+    
+}
