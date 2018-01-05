15edde14f469a9d165b7f2c1d1b88097aae107a8
Susan Lin <SusanLin@Susans-MacBook-Pro-2.local>
2018-01-05 12:22:08 -0500

    edit to woo

--- v1/Bee.java
+++ v1/Bee.java
@@ -1,14 +1,13 @@
 /*********************************
 class Bee extends Student
 **********************************/
-public class Bee extends Student
-{
-  public Bee() {
-	   intel = 30;
-	   friendCount = 7;
-     average = 100.0;
-     sleep = 75;
-     mental = 75;
-     physical = 30;
-   }
- }
+public class Bee extends Student{
+    public Bee() {
+	intel = 30;
+	friendCount = 7;
+	average = 100.0;
+	sleep = 75;
+	mental = 75;
+	physical = 30;
+    }
+}
--- v1/Scholar.java
+++ v1/Scholar.java
@@ -1,11 +1,10 @@
-public class Jock extends Student
-{
-  public Bee() {
-	   intel = 100;
-	   friendCount = 1;
-     average = 100.0;
-     sleep = 40;
-     mental = 75;
-     physical = 25;
-   }
+public class Jock extends Student {
+    public Bee() {
+	intel = 100;
+	friendCount = 1;
+	average = 100.0;
+	sleep = 40;
+	mental = 75;
+	physical = 25;
+    }
  }
