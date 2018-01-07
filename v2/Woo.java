289fb3947ef2db8422f2b563ee7fc1cdddc6a91c
Eric Wong <ewong11@stuy.edu>
2018-01-07 14:44:58 -0500

    made Woo current with changes

--- v2/Woo.java
+++ v2/Woo.java
@@ -9,11 +9,12 @@ public class Woo {
 
     public int days;
     public double luck;
+    private int score;
 
     private InputStreamReader isr;
     private BufferedReader in;
 
-    final String[] NAMES = {"Kevin", "Gabby"};
+    final String[] NAMES = {"Joe", "Gabby"}; //possible names for NPCS
 
 //---------------------------------------------------------
 
@@ -31,22 +32,32 @@ public void newGame() {
   String name;
   //these variables will determine the type of person you are
   int intelligence = 0;
-  int athletic = 0;
   int social = 0;
   s = "Welcome to StuyLife: Freshman Frenzy! \n";
-  s += "\t To start, let us figure out what type of person you are!";
+  s += "\n But first, let us know your name: \n";
+  System.out.println(s);
+
+  try {
+    name = in.readLine();
+  }
+  catch ( IOException e ) { }
+
+  //we can add a check here at the end aka "are you sure?"
+  System.out.println("Cool, nice to meet you, " + name + "!");
+  s = "\n To start, let us figure out what type of person you are!";
+  System.out.println(s);
 
   //ADD QUIZ HERE
 
   // makes the player one of three characters
-  if(Math.max (Math.max(intelligence, athletic), social)) = intelligence {
+  if(Math.max(intelligence, social) == intelligence) {
     player = new Scholar();
   }
-  else if(Math.max (Math.max(intelligence, athletic), social)) = athletic {
-    player = new Jock();
+  else if(Math.max(intelligence, social) == social) {
+    player = new Popular();
   }
   else {
-    player = new Bee();
+    player = new Normal();
   }
 
 }
