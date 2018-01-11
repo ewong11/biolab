2c6243a9d9dc80cf8919cd5c20525abc5d5b904b
Susan Lin <slin15@stuy.edu>
2018-01-11 08:04:40 -0500

    added to ImpressCrush

--- v4/ImpressCrush.java
+++ v4/ImpressCrush.java
@@ -1,7 +1,20 @@
-public class ImpressCrush{
+import import java.io.*;
+import java.util.*;
 
-  public static void main(String[] args){
-    System.out.println("Hello! Welcome. I am your mind. Look...");
-}
+public class ImpressCrush {
+    public int randNum;
+    public int guessesUsed;
+    public int totalGuesses;
 
+    public ImpressCrush() {
+	randNum = (int) (Math.random() * 100);
+	guessesUsed = 0;
+	totalGuesses = 7; 
+	
+    } 
+
+    public static void main(String[] args){
+	System.out.println("Hello! Welcome. I am your mind. Look... there's your crush! Say hi!");
+    
+    }
 }
