e713a59f61b292b95bd84e4863d40e67552e5727
Susan Lin <slin15@stuy.edu>
2018-01-12 13:54:15 -0500

    added import to all events

--- v4/Social.java
+++ v4/Social.java
@@ -1,9 +1,13 @@
+import java.io.*;
+import java.util.*;
+
 public class Social {
     public static void friend() {
     }
     public static void eatOut() {
     }
     public static void birthday() {
+	
     }
     public static void rumors() {
     }
