08f1cef5ea5a2ad0cfc54badae11c323566427c7
Susan Lin <slin15@stuy.edu>
2018-01-13 15:36:06 -0500

    skele in other

--- v5/Other.java
+++ v5/Other.java
@@ -2,13 +2,96 @@ import java.io.*;
 import java.util.*;
 
 public class Other {
+    private static InputStreamReader isr;
+    private static BufferedReader in;
+    
     public static void MTA() {
+	isr = new InputStreamReader(System.in);
+	in = new BufferedReader( isr);
+	String out = "";
+	int ans = 0;
+
+	out += "";
+	out += "";
+	out += "";
+	out += "";
+	try {
+	    ans = Integer.parseInt(in.readLine()); 
+	}
+	catch (IOException e) {}
+
+	if (ans == 1) {
+	}
+	else if (ans == 2) {
+	}
+	else {
+	} 
     }
     public static void allNighter() {
+	isr = new InputStreamReader(System.in);
+	in = new BufferedReader( isr);
+	String out = "";
+	int ans = 0;
+
+	out += "";
+	out += "";
+	out += "";
+	out += "";
+	try {
+	    ans = Integer.parseInt(in.readLine()); 
+	}
+	catch (IOException e) {}
+
+	if (ans == 1) {
+	}
+	else if (ans == 2) {
+	}
+	else {
+	} 
     }
     public static void soulCycle() {
+	isr = new InputStreamReader(System.in);
+	in = new BufferedReader( isr);
+	String out = "";
+	int ans = 0;
+
+	out += "";
+	out += "";
+	out += "";
+	out += "";
+	try {
+	    ans = Integer.parseInt(in.readLine()); 
+	}
+	catch (IOException e) {}
+
+	if (ans == 1) {
+	}
+	else if (ans == 2) {
+	}
+	else {
+	} 
     }
     public static void urbex() {
+	isr = new InputStreamReader(System.in);
+	in = new BufferedReader( isr);
+	String out = "";
+	int ans = 0;
+
+	out += "";
+	out += "";
+	out += "";
+	out += "";
+	try {
+	    ans = Integer.parseInt(in.readLine()); 
+	}
+	catch (IOException e) {}
+
+	if (ans == 1) {
+	}
+	else if (ans == 2) {
+	}
+	else {
+	} 
     }
     
 } 
