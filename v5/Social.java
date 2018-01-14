681efa362432f90813067c7031aff063b7d3e383
Kaitlin <kaitlin.wan@gmail.com>
2018-01-14 17:49:43 -0500

    Social Updates -K

--- v5/Social.java
+++ v5/Social.java
@@ -15,132 +15,182 @@ public class Social {
       Friend currFriend = Woo.yourFriends[choiceF];
       //Which FriendEvent to do?
       double choice = Math.random();
+
+      //Friend Event 1
       if(choice < 0.33){
-	    String s = "Your friend, " + currFriend +  " waved at you in the hallway! \n";
-	    s+= "\t What would you like to do? \n";
-	    s+= "\t1: Pretend you didn't notice \n";
-	    s+= "\t2: Wave Back!\n";
-	    s+= "\t3: Stop and Chat\n";
-	    System.out.println(s);
-      int ans = 0;
+	       String s = "Your friend, " + currFriend +  " waved at you in the hallway! \n";
+         s+= "\t What would you like to do? \n";
+         s+= "\t1: Pretend you didn't notice \n";
+         s+= "\t2: Wave Back!\n";
+         s+= "\t3: Stop and Chat\n";
+         System.out.println(s);
+         int ans = 0;
 
-      try {
-        ans = Integer.parseInt( in.readLine() );
-        if (ans == 1){
-          System.out.println("" + currFriend + " is a little offended and will\nremember your actions....");
-          currFriend.friendship -= 0.5;
-        }
-        else if (ans == 2){
-          System.out.println(currFriend + " feels a little closer to you now");
-          currFriend.friendship += 1;
-        }
+         try {
+           ans = Integer.parseInt( in.readLine() );
+           if (ans == 1){
+             System.out.println("" + currFriend + " is a little offended and will\nremember your actions....");
+             currFriend.friendship -= 0.5;
+           }
+           else if (ans == 2){
+             System.out.println(currFriend + " feels a little closer to you now");
+             currFriend.friendship += 1;
+           }
 
-        else{
-          System.out.println(currFriend + " loves the conversation! \nYour friendship shoots up but you're late to class....");
-          player.average -= 1;
+           else{
+             System.out.println(currFriend + " loves the conversation! \nYour friendship shoots up but you're late to class....");
+             player.average -= 1;
+           }
+         }
+      catch ( IOException e ){ }
       }
-      }
-      catch ( IOException e ) { }
 
+      //Friend Event 2
+      else if (choice < 0.66) {
+        String s = currFriend + " meets you at your locker and asks if you \nwant to go to help them with their homework after school.";
+	      s+= "\n What do you do? \n";
+	      s+= "\t1: No. I can't sorry. I have a lot of work to do\n";
+        s+= "\t2: Sure! I gotcha!\n";
+	      s+= "\t3: You can just copy mine!\n";
+	      System.out.println(s);
+        int ans = 0;
 
-    }
-    else if (choice < 0.66) {
-      String s = currFriend + " meets you at your locker and asks if you \nwant to go to help them with their homework after school.";
-	    s+= "\n What do you do? \n";
-	    s+= "\t1: No. I can't sorry. I have a lot of work to do\n";
-	    s+= "\t2: Sure! I gotcha!\n";
-	    s+= "\t3: You can just copy mine!\n";
-	    System.out.println(s);
-
-      int ans = 0;
-
-      try {
-        ans = Integer.parseInt( in.readLine() );
-        if (ans == 1){
-          System.out.println(currFriend + " is a little offended and will \nremember your actions....");
-          currFriend.friendship -= 1;
-        }
-        else if (ans == 2){
-          System.out.println(currFriend + " finishes their homework and gets 100!\nThey owe it all to you :)");
-          currFriend.friendship += 1.5;
-        }
-
-        else{
-          double caughtChance = Math.random();
-          if(caughtChance < 0.3){
-          System.out.println(currFriend + " gets caught copying your homework... \nYour average goes down 5 points but you and " + currFriend + " are closer than ever!");
-          player.average -= 5;
-          currFriend.friendship += 2;
-        }
-        else{
-          System.out.println(currFriend + " appreciates you.");
-          currFriend.friendship += 2;
-        }
-      }
-      }
-      catch ( IOException e ) { }
-
-    }
-    else{
-      String s = currFriend + " has a new significant other...";
-	    s+= "\nRecently, you've been neglected by the couple";
-	    s+= "\nand when you do get together, you're the third wheel.";
-	    s+= "\nWhat do you do?";
-	    s+= "\t1: Confront " + currFriend + "\n";
-	    s+= "\t2: Stop hanging out with them all together\n";
-	    s+= "\t3: You're not bothered by this.\n";
-	    System.out.println(s);
-
-      int ans = 0;
-
-      try {
-        ans = Integer.parseInt( in.readLine() );
-        if (ans == 2){
-          System.out.println(currFriend + " is a little offended and will \nremember your actions....");
-<<<<<<< HEAD
-          if (currFriend.friendship < 1){
-            currFriend.friendship = 0;
-          }
-          else
+        try {
+          ans = Integer.parseInt( in.readLine() );
+          if (ans == 1){
+            System.out.println(currFriend + " is a little offended and will \nremember your actions....");
             currFriend.friendship -= 1;
-=======
-          if (currFriend.friendship < 1) {
-            currFriend.friendship = 0;
           }
-          else
-            currFriend.friendship -= .3;
->>>>>>> a3bc23a7e87fe2f0e3996333f214a7bf5e1d652c
-        }
-        else if (ans == 3) {
-          System.out.println("Nothing Happens.");
-        }
+          else if (ans == 2){
+            System.out.println(currFriend + " finishes their homework and gets 100!\nThey owe it all to you :)");
+            currFriend.friendship += 1.5;
+          }
 
-        else{
-          double caughtChance = Math.random();
-          if(currFriend.friendship < 2){
-          System.out.println(currFriend + " understands your situation. \nYour friendship goes up!");
-          currFriend.friendship += 0.5;
+          else{
+            double caughtChance = Math.random();
+            //Get caught copying!!
+            if(caughtChance < 0.3){
+              System.out.println(currFriend + " gets caught copying your homework... \nYour average goes down 5 points but you and " + currFriend + " are closer than ever!");
+              player.average -= 5;
+              currFriend.friendship += 2;
+            }
+            //Just increases.
+            else{
+              System.out.println(currFriend + " appreciates you.");
+              currFriend.friendship += 2;
+            }
+          }
         }
-        else{
-          System.out.println(currFriend + " is angered...");
-          currFriend.friendship -= 1;
-        }
+      catch ( IOException e ) { }
       }
+        else{
+          String s = currFriend + " has a new significant other...";
+	        s+= "\nRecently, you've been neglected by the couple";
+	        s+= "\nand when you do get together, you're the third wheel.";
+	        s+= "\nWhat do you do?";
+	        s+= "\t1: Confront " + currFriend + "\n";
+          s+= "\t2: Stop hanging out with them all together\n";
+	        s+= "\t3: You're not bothered by this.\n";
+	        System.out.println(s);
+
+          int ans = 0;
+
+          try {
+            ans = Integer.parseInt( in.readLine() );
+            if (ans == 2){
+              System.out.println(currFriend + " is a little offended and will \nremember your actions....");
+              if (currFriend.friendship < 1) {
+                currFriend.friendship = 0;
+              }
+              else{
+                currFriend.friendship -= .3;
+              }
+            }
+            else if (ans == 3) {
+              System.out.println("Nothing Happens.");
+            }
+
+            else{
+              double caughtChance = Math.random();
+              if(currFriend.friendship < 2){
+                System.out.println(currFriend + " understands your situation. \nYour friendship goes up!");
+                currFriend.friendship += 0.5;
+              }
+              else{
+                System.out.println(currFriend + " is angered...");
+                currFriend.friendship -= 1;
+              }
+            }
+          }
+        catch ( IOException e ) { }
+      }
+    }
+
+    public static void AllFriendsChange(String change, double amt){
+      if(change.equals("add")){
+        Woo.friend1.friendship += amt;
+        Woo.friend2.friendship += amt;
+        Woo.friend3.friendship += amt;
+      }
+      else{
+        Woo.friend1.friendship -= amt;
+        Woo.friend2.friendship -= amt;
+        Woo.friend3.friendship -= amt;
+      }
+    }
+
+    public static void eatOut(Student p) {
+      isr = new InputStreamReader( System.in );
+      in = new BufferedReader( isr );
+
+	    String s = "EATING OUT TONIGHT!!!?!";
+	    s+= "\nAll your friends want to go out to eat tonight!";
+	    s+= "\nThey want you to come along as well!\n";
+	    s+= "\nYou have a lot of homework... What do you do?\n";
+	    s+= "\t1: You go home. Grades are more important today!\n";
+	    s+= "\t2: HECK YEAH! LET'S HAVE FUNNN TONIGHT!\n";
+	    s+= "\t3: You have other plans today!\n";
+	    System.out.println(s);
+
+      int ans = 0;
+      try {
+        ans = Integer.parseInt( in.readLine() );
+        if (ans == 1){
+          System.out.println("You studied hard that night and finished all your homework to your best possibility!");
+          System.out.println("Your average increases a little!");
+          p.average += 0.5;
+        }
+        else if (ans == 2){
+          System.out.println("It was SUCH a fun night :) However, you dont finish ANY of your homework... ");
+          System.out.println("Average does down slightly but friendship all goes up!");
+          p.average =+ 0.5;
+          AllFriendsChange("add", 0.3);
+        }
+        else{
+          int newOne = 0;
+          newOne = Integer.parseInt(in.readLine());
+          System.out.println("What are your new plans?");
+          System.out.println("\t1: Study Session @ AIS tutoring");
+          System.out.println("\t2: Going to sleep the second you get home!");
+          if(newOne == 1){
+            System.out.println("You ACE your test tomorrow!! WOoo!");
+            System.out.println("Grades go up... Friends go down :(");
+            p.average += .5;
+            AllFriendsChange("sub", 0.3);
+          }
+          else{
+            System.out.println("The best sleep yet!");
+            System.out.println("You wake up to texts from group chat texts \n");
+            System.out.println("\t: Can't believe " + p.name + " didn't come today." );
+            System.out.println("\t: At least now I know who my real friends are!")
+            AllFriendsChange("sub", 0.3);
+            p.sleep += 2;
+          }
+        }
       }
       catch ( IOException e ) { }
     }
 
-    }
-    public static void eatOut() {
-	String s = "EATING OUT TONIGHT!!!?!";
-	s+= "\n";
-	s+= "\t \n";
-	s+= "\t \n";
-	s+= "\t1: \n";
-	s+= "\t2: \n";
-	s+= "\t3: \n";
-	System.out.println(s);
-    }
     public static void birthday() {
       /*String s = "";
       s+= "HAPPY BIRTHDAY BUD! *cue scanner song\n";
@@ -149,19 +199,20 @@ public class Social {
       s+= "\t1: play hooky and go to Wok Wok! \n";
       s+= "\t2: do nothing. school is still a thing for you :( \n";
       s+= "\t3: Convince your teacher not to assign you homework\n";
-	System.out.println(s);
+	     System.out.println(s);
 
-	String ans = in.readLine(); */
-    }
+	      String ans = in.readLine(); */
+      }
+
     public static void rumors() {
-	String s = "THERE'S A RUMOR GOING AROUND?!";
-	s+= "\n";
-	s+= "\t \n";
-	s+= "\t \n";
-	s+= "\t1: \n";
-	s+= "\t2: \n";
-	s+= "\t3: \n";
-	System.out.println(s);
-    }
+	     String s = "THERE'S A RUMOR GOING AROUND?!";
+	     s+= "\n";
+	     s+= "\t \n";
+       s+= "\t \n";
+	     s+= "\t1: \n";
+       s+= "\t2: \n";
+       s+= "\t3: \n";
+       System.out.println(s);
+     }
 
 }
