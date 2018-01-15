ff0383970c6dacced968f1e93c1e990a62965de3
Susan Lin <slin15@stuy.edu>
2018-01-14 21:41:13 -0500

    fixed bug in other

--- v5/Other.java
+++ v5/Other.java
@@ -10,7 +10,7 @@ public class Other {
 	in = new BufferedReader( isr);
 	String out = "";
 	int ans = 0;
-
+	
 	out += a.name + ", the train is running late! What's your plan?\n";
 	out += "\t1: take a cab. my life revolves around school.\n";
 	out += "\t2: wait it out. the train will come eventually.\n";
@@ -20,7 +20,7 @@ public class Other {
 	    ans = Integer.parseInt(in.readLine());
 	}
 	catch (IOException e) {}
-
+	
 	if (ans == 1) {
 	    out = "the morning traffic holds the cab driver up. ";
 	    out += "you have 5 minutes left and you're at canal street.";
@@ -33,7 +33,7 @@ public class Other {
 		ans = Integer.parseInt(in.readLine());
 	    }
 	    catch (IOException e) {}
-
+	    
 	    if (ans == 1) {
 	        out = "okay, on the way, you catch a whiff of ferry's and coffee. ";
 		out += "do you stop to buy?\n";
@@ -85,15 +85,12 @@ public class Other {
 		} 
 	    }
 	    else {
-<<<<<<< HEAD
 		System.out.println("you had a great day eating food and having fun buuuut your mom gets a call and now you're in trouble\n");
 		a.charisma -= 1;
 		a.average -= 1; 
 	    } 
-=======
-	    }
->>>>>>> 8dfdf23376b5b50cec5212b8482dd97bcf586441
 	}
+   
 	else if (ans == 2) {
 	    out = "okay. so you're already 15 minutes late when you get to tribeca bridge. ";
 	    out += "do you decide to skip first period and get food? \n";
@@ -119,7 +116,6 @@ public class Other {
 		System.out.println("welp. just another day i guess.");
 	}
 	else {
-<<<<<<< HEAD
 	    out = "okay, you're at Times Square 42nd on the delayed 2 train. ";
 	    out += "which line do you take to get to Stuy?\n";
 	    out += "\t1: the ACE\n";
@@ -133,7 +129,7 @@ public class Other {
 	    catch (IOException e) {}
 
 	    if (ans == 1) {
-	        System.out.println("the ACE is even more delayed, if that's possible, and you get to school an hour late.\n");
+		System.out.println("the ACE is even more delayed, if that's possible, and you get to school an hour late.\n");
 		a.average -= 1;
 		a.mental -= 1;
 		a.charisma -= 1; 
@@ -173,14 +169,11 @@ public class Other {
 		System.out.println("you're late, but not as late as you could've been so you decide to just go on with the day.\n");
 		a.charisma += 1;
 		a.physical += 1; 
-	    } 	    
-	} 
-=======
+	    } 
 	}
->>>>>>> 8dfdf23376b5b50cec5212b8482dd97bcf586441
     }
-public static void allNighter(Student a) {
-  boolean question = false;
+    public static void allNighter(Student a) {
+	boolean question = false;
 	isr = new InputStreamReader(System.in);
 	in = new BufferedReader( isr);
 	String out = "";
@@ -188,43 +181,43 @@ public static void allNighter(Student a) {
 	out = "Projects are looming.";
 	out += "\nTests are coming up";
 	out += "\nYou have homework you didn't do.";
-  if (Woo.flix == true) {
-	out += "\nOh why did I watch binge Netflix?";
-}
-  System.out.println(out);
-while (question == false) {
-out = "\n1. You can pull an all-nighter and finish all your work OR";
-out += "\n2. You can just sleep, its a lost cause anyways";
-System.out.println(out);
-	try {
-	    ans = Integer.parseInt(in.readLine());
+	if (Woo.flix == true) {
+	    out += "\nOh why did I watch binge Netflix?";
 	}
-	catch (IOException e) {}
+	System.out.println(out);
+	while (question == false) {
+	    out = "\n1. You can pull an all-nighter and finish all your work OR";
+	    out += "\n2. You can just sleep, its a lost cause anyways";
+	    System.out.println(out);
+	    try {
+		ans = Integer.parseInt(in.readLine());
+	    }
+	    catch (IOException e) {}
 
-	if (ans == 1) {
-    out = "That was a long night, but you finally got everything done!";
-    out += "\nUnfortunately, you got no sleep and you won't be able to focus in class.";
-    a.sleep = 1;
-    a.physical -= 20;
-    a.mental -= 20;
-    a.average += 3;
-    question = true;
+	    if (ans == 1) {
+		out = "That was a long night, but you finally got everything done!";
+		out += "\nUnfortunately, you got no sleep and you won't be able to focus in class.";
+		a.sleep = 1;
+		a.physical -= 20;
+		a.mental -= 20;
+		a.average += 3;
+		question = true;
+	    }
+	    else if (ans == 2) {
+		out = "Yeah, sleeping was definitely the right choice! ";
+		out += "\nYou feel refreshed and invigorated, but your grades take a hit";
+		a.sleep += 3;
+		a.physical += 20;
+		a.mental += 20;
+		a.average -= 3;
+		question = true;
+	    }
+	    else {
+		out = "make a choice, silly";
+	    }
+	    System.out.println(out);
 	}
-	else if (ans == 2) {
-    out = "Yeah, sleeping was definitely the right choice! ";
-    out += "\nYou feel refreshed and invigorated, but your grades take a hit";
-    a.sleep += 3;
-    a.physical += 20;
-    a.mental += 20;
-    a.average -= 3;
-    question = true;
-	}
-	else {
-    out = "make a choice, silly";
-	}
-  System.out.println(out);
     }
-  }
     public static void soulCycle(Student a) {
 	isr = new InputStreamReader(System.in);
 	in = new BufferedReader( isr);
@@ -269,5 +262,4 @@ System.out.println(out);
 	else {
 	}
     }
-
 }
