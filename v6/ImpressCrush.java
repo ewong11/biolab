1786c211300e2d7858912ae98077defaa1d18324
Kaitlin <kaitlin.wan@gmail.com>
2018-01-15 19:01:15 -0500

    ImpressCrush

--- v6/ImpressCrush.java
+++ v6/ImpressCrush.java
@@ -7,14 +7,18 @@ public class ImpressCrush {
     public int totalGuesses;
 
     public ImpressCrush() {
-	randNum = (int) (Math.random() * 100);
-	guessesUsed = 0;
-	totalGuesses = 7; 
-	
-    } 
+      randNum = (int) (Math.random() * 100);
+	    guessesUsed = 0;
+      totalGuesses = 7;
+
+    }
+
+    public static void PlayGame(){
+      System.out.println("Hello! Welcome. I am your mind.\nLook... there's your crush! Say hi!");
+      System.out.println("");
+    }
 
     public static void main(String[] args){
-	System.out.println("Hello! Welcome. I am your mind. Look... there's your crush! Say hi!");
-    
+
     }
 }
